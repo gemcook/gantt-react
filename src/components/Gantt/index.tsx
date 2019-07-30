6fe721011b075c6fa144c772497e5b0b86fbdc1b
Sou Nishimura <s.nishimura@gemcook.com>
2019-07-30 11:47:04 +0900

    [update] b-303 useEffectの処理の分離

--- src/components/Gantt/index.tsx
+++ src/components/Gantt/index.tsx
@@ -10,27 +10,13 @@ const ReactGantt: React.FC<GanttProps> = props => {
   const ganttRef = useRef(null);
   const [gantt, setGantt] = useState();
 
+  // TODO hooksファイルを作成し、社内の最新の構成に合わせる
   useEffect(() => {
-    // キャメルケースで受け取った値をスネークケースにする。
-    const options = collection.toSnakeKeys(props.options);
-    const tasks = collection.toSnakeKeys(props.tasks);
+    if (!gantt) {
+      // キャメルケースで受け取った値をスネークケースにする。
+      const options = collection.toSnakeKeys(props.options);
+      const tasks = collection.toSnakeKeys(props.tasks);
 
-    // もしガントが表示済みなら更新する・存在しなければ新しく作成する
-    if (gantt) {
-      // TODO オプションをオブジェクトで受け取らないように修正する
-      // NOTE ganttを再生成する関数
-      gantt.refresh(tasks, {
-        ...options,
-        select_day: props.selectDay,
-        on_click: props.onClick,
-        on_date_change: props.onDateChange,
-        on_progress_change: props.onProgressChange,
-        on_view_change: props.onViewChange,
-        on_contextmenu: props.onContextMenu,
-        on_gantt_contextmenu: props.onGanttContextMenu,
-      });
-    } else {
-      // TODO オプションをオブジェクトで受け取らないように修正する
       // NOTE 新しいガントを作成する
       const ganttInstance = new Gantt(ganttRef.current, tasks, {
         ...options,
@@ -46,7 +32,23 @@ const ReactGantt: React.FC<GanttProps> = props => {
       // Ganttインスタンスを保持する
       setGantt(ganttInstance);
     }
-  }, [props, gantt]);
+  }, [gantt, props]);
+
+  // TODO hooksファイルを作成し、社内の最新の構成に合わせる
+  useEffect(() => {
+    // キャメルケースで受け取った値をスネークケースにする。
+    const options = collection.toSnakeKeys(props.options);
+    const tasks = collection.toSnakeKeys(props.tasks);
+
+    // もしガントが表示済みなら更新する
+    if (gantt) {
+      // NOTE ganttを再生成する関数
+      gantt.refresh(tasks, {
+        ...options,
+        select_day: props.selectDay,
+      });
+    }
+  }, [props.tasks, gantt, props.options, props.selectDay]);
 
   return (
     <div className="gc__frappe-gantt-react">
